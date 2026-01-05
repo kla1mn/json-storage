98a8c2646f06f82f0a27e840a22b420b3505a990
Моховиков Максим Дмитриевич <mokhovikov@tochka.com>
2026-01-05 12:53:52 +0500

    feat: добавил контекстный менеджер

--- json_storage/repositories/elastic_search.py
+++ json_storage/repositories/elastic_search.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import uuid
+from contextlib import asynccontextmanager
 from dataclasses import dataclass, field
-from typing import Any, Optional, ClassVar
+from typing import Any, Optional, ClassVar, AsyncGenerator
 from json_storage.settings import settings
 from elasticsearch import AsyncElasticsearch, NotFoundError
 
@@ -14,14 +15,19 @@ MappingsType = dict[str, Any]
 @dataclass
 class ElasticSearchDBRepository:
     NAMESPACES: ClassVar[dict[str, str]] = {}
-    REINDEX_NAMESPACE: ClassVar[set[str]] = set()
+    REINDEX_NAMESPACES: ClassVar[set[str]] = set()
     url: str
     _client: AsyncElasticsearch | None = field(init=False, default=None)
 
-    def _get_client(self) -> AsyncElasticsearch:
-        if self._client is None:
-            self._client = AsyncElasticsearch(self.url)
-        return self._client
+    @asynccontextmanager
+    async def get_client(self) -> AsyncGenerator[AsyncElasticsearch, None]:
+        try:
+            if self._client is None:
+                self._client = AsyncElasticsearch(self.url)
+            yield self._client
+        finally:
+            await self._client.close()
+            self._client = None
 
     @classmethod
     def _get_real_index(cls, namespace: str) -> str:
@@ -51,7 +57,8 @@ class ElasticSearchDBRepository:
             await client.indices.delete(index=new_index, ignore_unavailable=True)
             raise exc
         finally:
-            cls.REINDEX_NAMESPACE.remove(real_namespace)
+            cls.REINDEX_NAMESPACES.remove(real_namespace)
+            await client.close()
 
     async def create_or_update_index(
         self,
@@ -64,23 +71,23 @@ class ElasticSearchDBRepository:
         if mappings is None:
             mappings = {'mappings': {'dynamic': True, 'properties': {}}}
 
-        client = self._get_client()
+        async with self.get_client() as client:
 
-        try:
-            _exists = await client.indices.exists(index=real_index)
-            exists = bool(_exists.body) if hasattr(_exists, 'body') else bool(_exists)
-        except Exception:
-            exists = False
+            try:
+                _exists = await client.indices.exists(index=real_index)
+                exists = bool(_exists.body) if hasattr(_exists, 'body') else bool(_exists)
+            except Exception:
+                exists = False
 
-        if not exists:
-            await client.indices.create(index=real_index, body=mappings)
-            return
-        if namespace in self.REINDEX_NAMESPACE:
-            raise RuntimeError('Уже производится переиндексация')
+            if not exists:
+                await client.indices.create(index=real_index, body=mappings)
+                return
+            if namespace in self.REINDEX_NAMESPACES:
+                raise RuntimeError('Уже производится переиндексация')
 
-        from json_storage.tasks import reindex_namespace
-        self.REINDEX_NAMESPACE.add(namespace)
-        await reindex_namespace.kiq(index=real_index, real_namespace=namespace, mappings=mappings)
+            from json_storage.tasks import reindex_namespace
+            self.REINDEX_NAMESPACES.add(namespace)
+            await reindex_namespace.kiq(index=real_index, real_namespace=namespace, mappings=mappings)
 
     async def insert_document(
         self,
@@ -90,14 +97,14 @@ class ElasticSearchDBRepository:
         refresh: str | None = 'wait_for',
     ) -> bool:
         real_index = self._get_real_index(namespace)
-        client = self._get_client()
-        resp = await client.index(
-            index=real_index,
-            id=doc_id,
-            document=document,
-            refresh=refresh,
-        )
-        return resp.get('result') in ('created', 'updated')
+        async with self.get_client() as client:
+            resp = await client.index(
+                index=real_index,
+                id=doc_id,
+                document=document,
+                refresh=refresh,
+            )
+            return resp.get('result') in ('created', 'updated')
 
     async def get_document(
         self,
@@ -105,12 +112,12 @@ class ElasticSearchDBRepository:
         doc_id: str,
     ) -> Optional[JSONType]:
         real_index = self._get_real_index(namespace)
-        client = self._get_client()
-        try:
-            resp = await client.get(index=real_index, id=doc_id)
-        except NotFoundError:
-            return None
-        return resp.get('_source')
+        async with self.get_client() as client:
+            try:
+                resp = await client.get(index=real_index, id=doc_id)
+            except NotFoundError:
+                return None
+            return resp.get('_source')
 
     async def delete_document(
         self,
@@ -119,21 +126,21 @@ class ElasticSearchDBRepository:
         refresh: str | None = 'wait_for',
     ) -> bool:
         real_index = self._get_real_index(namespace)
-        client = self._get_client()
-        try:
-            resp = await client.delete(
-                index=real_index,
-                id=doc_id,
-                refresh=refresh,
-            )
-        except NotFoundError:
-            return False
-        return resp.get('result') == 'deleted'
+        async with self.get_client() as client:
+            try:
+                resp = await client.delete(
+                    index=real_index,
+                    id=doc_id,
+                    refresh=refresh,
+                )
+            except NotFoundError:
+                return False
+            return resp.get('result') == 'deleted'
 
     async def search_in_index(
         self, namespace: str, body: dict, size: int = 10, from_: int = 0
     ) -> list[Any]:
         real_index = self._get_real_index(namespace)
-        client = self._get_client()
-        resp = await client.search(index=real_index, body=body, size=size, from_=from_)
-        return [hit['_source'] for hit in resp.body['hits']['hits']]
+        async with self.get_client() as client:
+            resp = await client.search(index=real_index, body=body, size=size, from_=from_)
+            return [hit['_source'] for hit in resp.body['hits']['hits']]
